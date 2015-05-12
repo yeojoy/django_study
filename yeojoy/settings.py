ae40a10bb58608b4549478f396bbf89b568afa77
yeojoy <yeojoy.kim@gmail.com>
2015-05-12 10:45:49 +0900

    Model 추가, 실행 shell 명령어 추가
    Question, Choice Table 추가, 이를 admin에서 확인 할 수 있도록, admin페이지에도
    추가했다. settings.py에 polls를 추가했고 Timezone을 "서울"로 변경했다.

--- yeojoy/settings.py
+++ yeojoy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -87,7 +88,8 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
